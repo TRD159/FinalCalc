7497c41a8764baef89b4696735989f6143505073
TRD159 <rohan360k@gmail.com>
2019-02-28 10:32:36 -0600

    S

--- src/com/company/FinalFrame.java
+++ src/com/company/FinalFrame.java
@@ -3,45 +3,75 @@ package com.company;
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
+import java.text.Format;
+import java.text.NumberFormat;
 
 public class FinalFrame extends JFrame implements SwingConstants {
 
+    NumberFormat gradeForm = NumberFormat.getNumberInstance();
+
+    JLabel lbl_term = new JLabel("Term Grades");
+    JLabel lbl_terms = new JLabel("Terms: ");
     //Text Fields to take in the user's term grades
-    JTextField Term1 = new JTextField();
-    JTextField Term2 = new JTextField();
-    JTextField Term3 = new JTextField();
-    JTextField Term4 = new JTextField();
-    JTextField Term5 = new JTextField();
+
+    JLabel o = new JLabel("1: ");
+    JLabel tw = new JLabel("2: ");
+    JLabel th = new JLabel("3: ");
+    JLabel fo = new JLabel("4: ");
+    JLabel fi = new JLabel("5: ");
+
+    JButton Calculate = new JButton("Calculate!");
+    
+    JFormattedTextField Term1;
+    JFormattedTextField Term2;
+    JFormattedTextField Term3;
+    JFormattedTextField Term4;
+    JFormattedTextField Term5;
 
     Integer[] n = {1, 2, 3, 4, 5};
 
     //The number of terms applicable
     JComboBox<Integer> Terms = new JComboBox<>(n);
 
+    JFormattedTextField gradeWanted;
+
     //Button Group to hold all the radio buttons for the grade the user wants
-    ButtonGroup Grades = new ButtonGroup();
+    //ButtonGroup Grades = new ButtonGroup();
 
     //Two sliders to adjust the weight of the term grades and the final grades
-    JSlider TermW = new JSlider(SwingConstants.VERTICAL, 0, 100, 85);
-    JSlider FinalW = new JSlider(SwingConstants.VERTICAL, 0, 100, 15);
+    JSlider TermW = new JSlider(SwingConstants.HORIZONTAL, 0, 100, 85);
+    JSlider FinalW = new JSlider(SwingConstants.HORIZONTAL, 0, 100, 15);
 
     JLabel Term = new JLabel();
     JLabel Final = new JLabel();
 
-    char[] letterGrades = {'A', 'B', 'C', 'D'};
-    char[] gradeMods = {'+', ' ', '-'};
+    JLabel Wanted = new JLabel("Grade Wanted: ");
+    JLabel Needed = new JLabel();
+
+    Character[] gradeMods = {'+', ' ', '-'};
 
     public FinalFrame(String name) {
         super(name);
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1500, 1000);
+        gradeForm.setMaximumIntegerDigits(3);
+        gradeForm.setMaximumFractionDigits(2);
 
-        Term1.setBounds(50, 50, 225, 25);
-        Term2.setBounds(50, 100, 225, 25);
-        Term3.setBounds(50, 150, 225, 25);
-        Term4.setBounds(50, 200, 225, 25);
-        Term5.setBounds(50, 250, 225, 25);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(424, 293);
+
+        Term1 = new JFormattedTextField(gradeForm);
+        Term2 = new JFormattedTextField(gradeForm);
+        Term3 = new JFormattedTextField(gradeForm);
+        Term4 = new JFormattedTextField(gradeForm);
+        Term5 = new JFormattedTextField(gradeForm);
+
+        gradeWanted = new JFormattedTextField(gradeForm);
+
+        Term1.setBounds(50, 100, 50, 25);
+        Term2.setBounds(50, 130, 50, 25);
+        Term3.setBounds(50, 160, 50, 25);
+        Term4.setBounds(50, 190, 50, 25);
+        Term5.setBounds(50, 220, 50, 25);
 
         Term1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         Term2.setBorder(BorderFactory.createLineBorder(Color.BLACK));
@@ -49,22 +79,40 @@ public class FinalFrame extends JFrame implements SwingConstants {
         Term4.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         Term5.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
-        FinalW.setBounds(100, 350, 25, 100);
-        TermW.setBounds(125, 350, 25, 100);
+        FinalW.setBounds(0, 0, 200, 25);
+        TermW.setBounds(0, 25, 200, 25);
 
-        Final.setBounds(100, 450, 25, 50);
-        Term.setBounds(125, 450, 25, 50);
+        Final.setBounds(225, 0, 125, 25);
+        Term.setBounds(225, 25, 125, 25);
 
-        Final.setText("15");
-        Term.setText("85");
+        Final.setText("Final Weight: 15%");
+        Term.setText("Term Weight: 85%");
 
-        Terms.setBounds(300, 50, 100, 25);
+        lbl_term.setBounds(20, 0, 140, 145);
+        lbl_terms.setBounds(300, 75, 75, 25);
+
+        Wanted.setBounds(300, 150, 100, 25);
+        gradeWanted.setBounds(300, 175, 100, 25);
+
+        Needed.setBounds(150, 220, 150, 25);
+
+        o.setBounds(25, 100, 25, 25);
+        tw.setBounds(25, 130, 25, 25);
+        th.setBounds(25, 160, 25, 25);
+        fo.setBounds(25, 190, 25, 25);
+        fi.setBounds(25, 220, 25, 25);
+
+        Calculate.setBounds(150, 100, 100, 100);
+
+        Terms.setBounds(300, 100, 100, 25);
         Terms.setSelectedIndex(4);
         
         FinalW.addChangeListener(e -> FinalUpdate());
         TermW.addChangeListener(e -> TermUpdate());
 
         Terms.addActionListener(e -> TermsUpdate());
+
+        Calculate.addActionListener(e -> calc());
 
         setLayout(null);
 
@@ -82,14 +130,34 @@ public class FinalFrame extends JFrame implements SwingConstants {
 
         add(Terms);
 
+        add(Wanted);
+        add(gradeWanted);
+
+        add(Needed);
+
+        add(lbl_term);
+        add(lbl_terms);
+
+        add(o);
+        add(tw);
+        add(th);
+        add(fo);
+        add(fi);
+
+        add(Calculate);
+
         setVisible(true);
     }
 
     public void FinalUpdate() {
-        Final.setText(""+FinalW.getValue());
+        TermW.setValue(100 - FinalW.getValue());
+        Final.setText("Final Weight: "+FinalW.getValue() + "%");
+        Term.setText("Term Weight: "+TermW.getValue() + "%");
     }
     public void TermUpdate() {
-        Term.setText(""+TermW.getValue());
+        FinalW.setValue(100 - TermW.getValue());
+        Final.setText("Final Weight: "+FinalW.getValue() + "%");
+        Term.setText("Term Weight: "+TermW.getValue() + "%");
     }
 
     public void TermsUpdate() {
@@ -134,6 +202,60 @@ public class FinalFrame extends JFrame implements SwingConstants {
                 Term4.setEditable(true);
                 Term5.setEditable(true);
         }
+    }
+
+    public void calc() {
+        double t1, t2, t3, t4, t5;
+        double wanted;
+
+        try {
+            t1 = Double.parseDouble(Term1.getText());
+        } catch (NumberFormatException e) {
+            t1 = 0;
+        }
+        try {
+            t2 = Double.parseDouble(Term2.getText());
+        } catch (NumberFormatException e) {
+            t2 = 0;
+        }
+        try {
+            t3 = Double.parseDouble(Term3.getText());
+        } catch (NumberFormatException e) {
+            t3 = 0;
+        }
+        try {
+            t4 = Double.parseDouble(Term4.getText());
+        } catch (NumberFormatException e) {
+            t4 = 0;
+        }
+        try {
+            t5 = Double.parseDouble(Term5.getText());
+        } catch (NumberFormatException e) {
+            t5 = 0;
+        }
+        double tn = Terms.getSelectedIndex() + 1;
+
+        double ta = (t1+t2+t3+t4+t5)/tn;
+
+        try {
+            wanted = Double.parseDouble(gradeWanted.getText());
+        } catch (NumberFormatException e) {
+            wanted = 0;
+        }
+
+        double tw = TermW.getValue()/100.0;
+        double fw = FinalW.getValue()/100.0;
+
+        System.out.println(tw);
+
+        double gradeNeeded = -(((ta*tw)-wanted)/fw);
+
+        if(gradeNeeded == -0)
+            gradeNeeded *= -1;
+
+        String finalG = String.format("%.2f", gradeNeeded);
+
+        Needed.setText("Grade Needed: " + finalG);
     }
 
 }
